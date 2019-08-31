807aa6ff62179b899e8f953e47afae163775d77d
Big C <onetreearmy@gmail.com>
2019-09-01 00:31:13 +0300

    попробовал html парсер didom

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 require 'vendor/autoload.php';
 use GuzzleHttp\Client;
+use DiDom\Document;
 
 $client = New Client();
-$response = $client->post('https://exist.ru/Catalog/Goods/5/66');
-$resBody = (string)$response->getBody();
-echo $resBody;
+
+$response = $client->get('https://exist.ru/Catalog/Goods/5/66');
+
+$resBody = null;
+
+if ($response->getStatusCode() == 200) {
+    $resBody = (string)$response->getBody();
+} else {
+    echo "Некорректный код статуса" . $response->getStatusCode();
+}
+
+$document = new Document($resBody);
+$titles = $document->find('div.wrap p');
+foreach ($titles as $title) {
+    echo $title . PHP_EOL;
+}

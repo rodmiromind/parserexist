1a64a802a68df24f0432eff212ccbad5bb5b6aef
Big C <onetreearmy@gmail.com>
2019-09-01 23:35:05 +0300

    Получилось более-менее корректно распределить спарсенную инфу по столбцам csv. Добавил библиотеку league

--- index.php
+++ index.php
@@ -2,21 +2,50 @@
 require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use DiDom\Document;
+use League\Csv\Writer;
 
-$client = New Client();
+$arrayTitles = ['Title'];
+$arrayParams = ['Parameters'];
+$arrayPrices = ['Price'];
+//$arrayColumns = ['Title', 'Parameters', 'Price'];
 
-$response = $client->get('https://exist.ru/Catalog/Goods/5/66');
+for ($i = 1; $i <= 15; $i++) {
+    $client = New Client();
+    $response = $client->get('https://exist.ru/Catalog/Goods/5/66#&&p={$i}&F=');
 
-$resBody = null;
+    $resBody = null;
 
-if ($response->getStatusCode() == 200) {
-    $resBody = (string)$response->getBody();
-} else {
-    echo "Некорректный код статуса" . $response->getStatusCode();
+    if ($response->getStatusCode() == 200) {
+        $resBody = (string)$response->getBody();
+    } else {
+        echo "Некорректный ответ сервера. Код статуса" . $response->getStatusCode();
+    }
+
+    $document = new Document($resBody);
+
+    
+    $titles = $document->find('div.wrap p');
+    foreach ($titles as  $title) {
+        $arrayTitles[] = strip_tags($title);
+    }
+    
+    $params = $document->find('div.desc span');
+    foreach ($params as  $param) {
+        $arrayParams[] = strip_tags($param);
+    }
+
+    $prices = $document->find('div.ucatprcdiv span.ucatprc');
+    foreach ($prices as $price) {
+        $arrayPrices[] = strip_tags($price);
+    }
+    print_r($i);
 }
 
-$document = new Document($resBody);
-$titles = $document->find('div.wrap p');
-foreach ($titles as $title) {
-    echo $title . PHP_EOL;
-}
+for ($i = 0; $i < 15; $i++) {
+    $arrayForCsv[] = [$arrayTitles[$i], $arrayParams[$i], $arrayPrices[$i]];
+  }
+
+
+$writer = Writer::createFromPath('file.csv', 'w+');
+$writer->setDelimiter(';');
+$writer->insertAll($arrayForCsv);
